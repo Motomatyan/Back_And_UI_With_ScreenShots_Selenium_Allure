7c2913f271b6841a98c01adb4abce9c21832eced
Olga Skirda <olgask599@gmail.com>
2022-12-14 16:18:16 +0300

    Add allure and registration

---
+++ src/test/java/Users/DoRegister.java
@@ -0,0 +1,28 @@
+package Users;
+
+public class DoRegister {
+    private String email;
+    private String name;
+    private String password;
+
+    public DoRegister(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public DoRegister() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
--- src/test/java/Users/UsersTest.java
+++ src/test/java/Users/UsersTest.java
@@ -1,16 +1,37 @@
 package Users;
 
 import core.BaseSeleniumTest;
+import core.Specification;
 import helpers.UserTestValues;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class UsersTest extends BaseSeleniumTest {
+
+    public static final String URL3 = "http://users.bugred.ru/";
+    @DisplayName("Test with registration")
+
     @Test
     public void checkAuth(){
+        doRegister();
         SearchPage searchPage = new MainPage().auth(UserTestValues.TEST_EMAIL,UserTestValues.TEST_PASSWORD).search(UserTestValues.TEST_EMAIL);
         Assertions.assertEquals(searchPage.getEmail(),UserTestValues.TEST_EMAIL);
         Assertions.assertEquals(searchPage.getName(),UserTestValues.TEST_NAME);
+
+    }
+    @Step("DoRegister")
+
+    public void doRegister(){
+        Specification.installSpecification(Specification.requestSpec(URL3), Specification.responseSpec200());
+        DoRegister user = new DoRegister(UserTestValues.TEST_EMAIL,UserTestValues.TEST_NAME,UserTestValues.TEST_PASSWORD);
+        given()
+                .when()
+                .post("tasks/rest/doregister")
+                .then().log().all();
 
     }
 }
